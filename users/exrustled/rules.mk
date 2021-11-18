f02798a200744340fcb30fab49cd06bb1ab89c2a
Dhiyaa Ramadhan <exrustled@github.com>
2021-11-18 12:35:05 -0800

    updating personal keymap

--- users/exrustled/rules.mk
+++ users/exrustled/rules.mk
@@ -1,7 +1,7 @@
 MOUSEKEY_ENABLE = yes # Mouse keys
 EXTRAKEY_ENABLE = yes # Audio control and System control
 COMBO_ENABLE 	= yes # Enables combos
-NKRO_ENABLE		= yes # Enables N-key rollover?
+NKRO_ENABLE		= yes # Enables N-key rollover
 
 SRC += exrustled.c # keymap
 
